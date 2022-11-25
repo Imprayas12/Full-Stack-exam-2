5e1887d9b8cd6b5dc3998ba4c987b14ec4680c87
prayas gautam <prayas12gautam@gmail.com>
2022-11-25 22:58:08 +0530

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const xhr = new XMLHttpRequest;
 
-xhr.onload = function(){
+xhr.onload = function () {
     var data = JSON.parse(this.response);
     const movie_grid = document.getElementById('display-movies');
     movie_grid.innerHTML = ``;
-    for(var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         var newItem = document.createElement('img')
         newItem.src = data[i].show.image.medium;
         movie_grid.appendChild(newItem);
@@ -13,11 +13,46 @@ xhr.onload = function(){
 }
 
 
-document.getElementById('search-bar-btn').addEventListener('click',(e) => {
+document.getElementById('search-bar-btn').addEventListener('click', (e) => {
     let ele = document.getElementById('search-bar').value;
     document.getElementById('search-bar').value = "";
     const url = " https://api.tvmaze.com/search/shows?q=" + ele;
-    xhr.open("GET",url);
+    xhr.open("GET", url);
     e.preventDefault();
     xhr.send();
 })
+
+var themeColors = Array.from(document.querySelectorAll('[name = "theme"]'));
+const storeTheme = function (theme) {
+    localStorage.setItem('theme', theme);
+}
+
+const applyTheme = function () {
+    const activeTheme = localStorage.getItem('theme');
+    document.getElementById(activeTheme).checked = true;
+    // changeFont();
+}
+themeColors.forEach((themeOption) => {
+    themeOption.addEventListener('click', () => {
+        storeTheme(themeOption.id);
+    })
+})
+document.onload = applyTheme();
+// function changeFont() {
+//     var heading = document.getElementById('heading');
+//     if (document.getElementById('light').checked === true) {
+//         heading.style.color = "black";
+//     }
+//     if (document.getElementById('blue').checked === true) {
+//         heading.style.color = "white";
+//     }
+//     if (document.getElementById('orange').checked === true) {
+//         heading.style.color = "#e0e0e0";
+//     }
+//     if (document.getElementById('babyPink').checked === true) {
+//         heading.style.color = "black";
+//     }
+//     if (document.getElementById('dark').checked === true) {
+//         heading.style.color = "white";
+//     }
+// }
